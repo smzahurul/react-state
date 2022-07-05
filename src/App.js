6c40ea7d95aaab68b759ad217863230aaaa6b6ac
S M Zahurul Islam <zahurul.it@gmail.com>
2022-07-05 23:06:59 +0600

    React State

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import React, { useState } from 'react';  
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="reappear">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Learn React User State
-        </p>       
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Learn React User State</p>
         </header>
       </div>
       <div className="container">
-      <Counter></Counter>
+        <Counter></Counter>
       </div>
     </div>
-    
   );
 }
-function Counter() {
-const [state, setstate] = useState(0);
-const countManager=()=>setstate(state +1);
-  // const newCount=state + 1;
-  
+const ButtonStyles = {
+  margin: '10px',
+  padding:'10px',
+  backgroundColor: 'rgba(255, 255, 255)',
+  borderRadius: '5px',
 
-  return(
+};
+const ButtonStyles1 = {
+  margin: '10px',
+  padding:'10px',
+  backgroundColor: 'red(200, 252, 222)',
+  borderRadius: '5px',
+
+};
+function Counter() {
+  const [state, setState] = useState(0);
+  const countManager = () => setState(state + 1);
+  // const newCount=state + 1;
+  return (
     <div>
       <h2>Counte : {state}</h2>
-      <button onClick={()=>setstate(state +1)}>Add Now</button>
-      <button onClick={()=>setstate(state -1)}>Delete Now</button>
+      <button style={ButtonStyles}onMouseMove={() => setState(state + 1)}>Add Now</button>
+      <button style={ButtonStyles1} onClick={() => setState(state - 1)}>Delete Now</button>
     </div>
-  )
+  );
 }
 
 export default App;
